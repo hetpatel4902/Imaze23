4edf7dd37a6a64bf56df1bddd77ccc0d7838d136
hetpatel4902 <88403427+hetpatel4902@users.noreply.github.com>
2023-02-17 23:12:15 +0530

    Nodemailer updated

--- Auth/controllers/Users.js
+++ Auth/controllers/Users.js
@@ -1,80 +1,90 @@
 const express = require("express");
-const app=express();
-const User = require('../models/Users')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError,UnauthenticatedError} = require('../errors/index')
-const jwt = require('jsonwebtoken')
-const nodemailer = require('nodemailer')
+const app = express();
+const User = require("../models/Users");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors/index");
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
 
-const registerUsers = async (req,res) => {
-  const {name,email,password,college,enrno,qrlink}=req.body
-   if(!email || !name || !password || !college || !enrno ){
-     throw new BadRequestError('Please provide necessary credentials')
-   }
-  const user = await User.create(req.body)
-  const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({user:{name:user.name,id:user._id},token})
-}
-
-const forgotPasswordUsers = async (req,res) => {
-  const {email}=req.body;
-  if(!email){
-    throw new BadRequestError('Please provide email')
+const registerUsers = async (req, res) => {
+  const { name, email, password, college, enrno, qrlink } = req.body;
+  if (!email || !name || !password || !college || !enrno) {
+    throw new BadRequestError("Please provide necessary credentials");
   }
-  const otp = Math.floor(Math.random()*(10000-1000+1)+1000)
-  console.log(otp)
-  const user = await User.findOneAndUpdate({email:email},{otp:otp},{new:true,runValidators:true})
-  if(!user){
-    throw new BadRequestError('Please provide valid email')
+  const user = await User.create(req.body);
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.name, id: user._id }, token });
+};
+
+const forgotPasswordUsers = async (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    throw new BadRequestError("Please provide email");
+  }
+  const otp = Math.floor(Math.random() * (10000 - 1000 + 1) + 1000);
+  console.log(otp);
+  const user = await User.findOneAndUpdate(
+    { email: email },
+    { otp: otp },
+    { new: true, runValidators: true }
+  );
+  if (!user) {
+    throw new BadRequestError("Please provide valid email");
   }
 
   const transporter = nodemailer.createTransport({
-    host: "smtp-mail.outlook.com", // hostname
+    host: "smtp.gmail.com", // hostname
     secureConnection: false, // TLS requires secureConnection to be false
     port: 587, // port for secure SMTP
     tls: {
-       ciphers:'SSLv3'
+      ciphers: "SSLv3",
     },
     auth: {
-        user: 'ssip69@outlook.com',
-        pass: 'Password@69'
-    }
+      user: "hetpatel5542@gmail.com",
+      pass: "xivslyvrfcrgewtb",
+    },
   });
-  
+
   const mailOptions = {
-    from: '"IMAZE 23 " <ssip69@outlook.com>', // sender address (who sends)
+    from: '"Sachivalaya " <ssip69@outlook.com>', // sender address (who sends)
     to: `${email}`, // list of receivers (who receives)
-    subject: 'OTP for Reseting Your User App Password ', // Subject line
+    subject: "OTP for Reseting Your User App Password ", // Subject line
     text: `Your OTP for reseting the password for User app is ${otp}, please enter this OTP in your User app to reset your password.
 -Thanks,
-Team IMAZE 23  `, // plaintext body
+Team Sachivalaya  `, // plaintext body
   };
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      return console.log(error);
     }
 
-    res.status(StatusCodes.OK).json({otpsent:true})
+    res.status(StatusCodes.OK).json({ otpsent: true });
   });
-}
+};
 
-const loginUsers = async (req,res) => {
-  const {email,password} = req.body
-  if(!email || !password){
-    throw new BadRequestError('Please provide email and password')
+const loginUsers = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
   }
-  const user = await User.findOne({email})
-  if(!user){
-    throw new UnauthenticatedError('Invalid Credentials')
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid Credentials");
   }
-  const isPasswordCorrect = await user.comparePassword(password)
-  if(!isPasswordCorrect){
-    throw new UnauthenticatedError('Invalid Credentials')
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
   }
-  const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({user:{name:user.name,id:user._id},token})
-}
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .json({ user: { name: user.name, id: user._id }, token });
+};
 
 module.exports = {
-  registerUsers,forgotPasswordUsers,loginUsers
-}
+  registerUsers,
+  forgotPasswordUsers,
+  loginUsers,
+};
