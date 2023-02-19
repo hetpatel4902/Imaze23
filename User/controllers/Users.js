944fd8283efb1381ec2f5142758b2afed29196b3
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-19 21:36:38 +0530

    participation certificate completed

--- User/controllers/Users.js
+++ User/controllers/Users.js
@@ -3,9 +3,10 @@ const Event = require("../models/Event");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors/index");
 const nodemailer = require("nodemailer");
-const ejs = require("ejs");
-const pdf = require("html-pdf");
 const fs = require("fs");
+const path = require("path");
+// Importing modules
+const pdfkit = require("pdfkit");
 
 const getOneEvent = async (req, res) => {};
 const getAllEvents = async (req, res) => {
@@ -26,25 +27,42 @@ const getUserEvents = async (req, res) => {};
 
 const getCertificate = async (req, res) => {
   const { uid, eid } = req.params;
-  const data = { username: "pratham", ename: "techy ludo", day: "18" };
-  const filepathname = __dirname + "/certificate.ejs";
-  const htmlstring = fs.readFileSync(filepathname).toString();
-  const options = {
-    format: "Letter",
-    childProcessOptions: {
-      env: {
-        OPENSSL_CONF: "/dev/null",
-      },
-    },
-  };
-  const ejsdata = ejs.render(htmlstring, data);
+  const user = await User.findOne({ _id: uid });
+  const event = await Event.findOne({ _id: eid });
+  // Create a document
+  const doc = new pdfkit();
+  // const doc = new PDFDocument();
+  // Saving the pdf file in root directory.
+  doc.pipe(fs.createWriteStream(`certificate-${user.name}-${event.name}.pdf`));
+  doc.image("./certificate.jpg", 0, 0, { width: 620, height: 800 });
+  const angle = Math.PI * 28.6;
+  doc.rotate(angle, { origin: [300, 240] });
+  doc.fontSize(17).text(user.name, 300, 240, {
+    width: 300,
+    align: "center",
+  });
+  doc.fontSize(17).text(event.name, 360, 299, {
+    width: 210,
+    align: "center",
+  });
+  doc.fontSize(15).text("18", 420, 381, {
+    width: 40,
+    align: "center",
+  });
+  doc.end();
   setTimeout(() => {
-    pdf.create(ejsdata, options).toFile(`certificate.pdf`, (err, response) => {
-      if (err) console.log(err);
-      console.log("File Generated");
-      res.status(200).json("success");
-    });
-  }, 2000);
+    var file = fs.createReadStream(
+      `./certificate-${user.name}-${event.name}.pdf`
+    );
+    var stat = fs.statSync(`./certificate-${user.name}-${event.name}.pdf`);
+    res.setHeader("Content-Length", stat.size);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=participation_certificate.pdf"
+    );
+    file.pipe(res);
+  }, 1000);
 };
 
 const checkDynamicCombo = async (req, res) => {};
