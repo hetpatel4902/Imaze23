6e15254c0021b57691992403130c5b0509c5c640
shahkandarp <102229699+shahkandarp@users.noreply.github.com>
2023-10-02 17:23:00 +0530

    added few more api for lead

--- Leads And Coordinator/routes/LeadRoute.js
+++ Leads And Coordinator/routes/LeadRoute.js
@@ -1,22 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const authMiddleware = require('../middleware/authentication')
 
-const {eventFetch,participantList,alreadyAttendedUser,updateAttendance,updateEvent,fetchLead,fetchWinners,updateWinners,searchUserEmail,showEventOfflineForUser,verifyEventOfflineOTP,showComboOfflineOTP,verifyComboOfflineOTP,eventParticipantExcel,eventAttendedExcel} = require('../controller/LeadController')
+const {eventFetch,participantList,alreadyAttendedUser,updateAttendance,updateEvent,fetchLead,fetchWinners,updateWinners,searchUserEmail,showEventOfflineForUser,verifyEventOfflineOTP,showComboOfflineOTP,verifyComboOfflineOTP,eventParticipantExcel,eventAttendedExcel,verifiedOfflineEvent,rejectOfflineEvent,getIndividualCulturalEvent,getAllCulturalEvents} = require('../controller/LeadController')
 
-router.route('/eventFetch/:eid').get(eventFetch)
-router.route('/participantList/:eid').get(participantList)
-router.route('/alreadyAttendedUser/:eid').get(alreadyAttendedUser)
-router.route('/updateAttendance/:eid').post(updateAttendance)
-router.route('/updateEvent/:eid').post(updateEvent)
-router.route('/fetchLead/:lid').get(fetchLead)
-router.route('/fetchWinners/:eid').get(fetchWinners)
-router.route('/updateWinners/:eid').post(updateWinners)
-router.route('/searchUserEmail').get(searchUserEmail)
+router.route('/eventFetch/:eid').get(authMiddleware,eventFetch)
+router.route('/participantList/:eid').get(authMiddleware,participantList)
+router.route('/alreadyAttendedUser/:eid').get(authMiddleware,alreadyAttendedUser)
+router.route('/updateAttendance/:eid').post(authMiddleware,updateAttendance)
+router.route('/updateEvent/:eid').post(authMiddleware,updateEvent)
+router.route('/fetchLead/:lid').get(authMiddleware,fetchLead)
+router.route('/fetchWinners/:eid').get(authMiddleware,fetchWinners)
+router.route('/updateWinners/:eid').post(authMiddleware,updateWinners)
+router.route('/searchUserEmail').get(authMiddleware,searchUserEmail)
 router.route('/showEventOfflineForUser').post(showEventOfflineForUser)
 router.route('/verifyEventOfflineOTP').post(verifyEventOfflineOTP)
 router.route('/showComboOfflineOTP').post(showComboOfflineOTP)
 router.route('/verifyComboOfflineOTP').post(verifyComboOfflineOTP)
-router.route('/eventParticipantExcel/:eid').get(eventParticipantExcel)
-router.route('/eventAttendedExcel/:eid').get(eventAttendedExcel)
+router.route('/eventParticipantExcel/:eid').get(authMiddleware,eventParticipantExcel)
+router.route('/eventAttendedExcel/:eid').get(authMiddleware,eventAttendedExcel)
+
+//it will veridy or reject entries of user on publicity screen
+router.route('/verifiedOfflineEvent').post(verifiedOfflineEvent)
+router.route('/rejectOfflineEvent').post(rejectOfflineEvent)
+
+//cultural screens
+router.route('/getAllCulturalEvents').get(getAllCulturalEvents)
+router.route('/getIndividualCulturalEvent/:cid').get(getIndividualCulturalEvent)
 
 module.exports = router
