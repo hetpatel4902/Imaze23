d629a984096aefa9c93d310dc9d9d7c7551d9f6a
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-10-10 01:30:17 +0530

    user:clean up

--- User/app.js
+++ User/app.js
@@ -29,23 +29,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-const Event = require("./models/Event");
-const UserEvent = require("./models/UserEvent");
-const Combos = require("./models/Combos");
-// const tech_events = require("./tech.json")
-// const non_tech_events = require("./non_tech.json")
-const staticcombos = require("./models/StaticCombo")
-// const ws =require("./Workshops.json")
-const cult = require("./cultural_solo.json");
-const Cultural = require("./models/Cultural");
-const FlagshipEvents = require("./models/FlagshipEvents");
-const cmbs = require("./combos.json")
+
 //routes user
-app.get("/clean",async(req,res)=>{
- 
-  const upd = await staticcombos.create(cmbs);
-  res.json("success")
-})
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/lead", leadRoute);
 app.use(notFoundMiddleware);
